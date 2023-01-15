558decd28f57270681b47ec0426596c45453e529
MartinLucena28 <martinlucena02@gmail.com>
2023-01-15 15:35:28 -0300

    FIRE

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+    projectId: 'frontendcml',
+    appId: '1:421082038802:web:e32850326d00d3147b4ecb',
+    storageBucket: 'frontendcml.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyBUXGOcIjC6Y2O0RuQOBcHQh8uNNCmqCps',
+    authDomain: 'frontendcml.firebaseapp.com',
+    messagingSenderId: '421082038802',
+  },
+
+    production: false,
+    URL: 'http://localhost:8080/'
+  };
