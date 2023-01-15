aa28d292512e0420c749bb96e171dbdceeacf68e
MartinLucena28 <martinlucena02@gmail.com>
2023-01-15 19:56:42 -0300

    main

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -10,16 +10,16 @@ import { NuevoUsuario } from '../model/nuevo-usuario';
 })
 export class AuthService {
 
-  authURL = 'https://backend-iedf.onrender.com/auth/';
+  URL = 'https://backend-iedf.onrender.com/auth/';
 
  
   constructor(private httpClient: HttpClient) { }
 
  public nuevo(nuevoUsuario: NuevoUsuario): Observable<any>{
-   return this.httpClient.post<any>(this.authURL + 'nuevo', nuevoUsuario);
+   return this.httpClient.post<any>(this.URL + 'nuevo', nuevoUsuario);
  }
 
  public login(loginUsuario: LoginUsuario): Observable<JwtDto>{
-   return this.httpClient.post<JwtDto>(this.authURL + 'login', loginUsuario)
+   return this.httpClient.post<JwtDto>(this.URL + 'login', loginUsuario)
  }
 }
